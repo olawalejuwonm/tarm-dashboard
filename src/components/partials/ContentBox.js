117d50a4d50c04bb473dc9e04881b33fc4909642
Adediran stephen <45730336+stephenkayade@users.noreply.github.com>
2021-04-08 15:46:54 +0100

    latst commit

--- src/components/partials/ContentBox.js
+++ src/components/partials/ContentBox.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const ContentBox = ({linkURL, title}) => {
     return(
         <>
-            <div className="">
+            <div className="d-flex justify-content-center align-items-center">
 
             <li><Link to={linkURL ? linkURL : null}>{title ? title : 'No title'}</Link></li>
             </div>
